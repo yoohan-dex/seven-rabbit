57021fe0544da72765333689d2067d992ed4ecd4
Yoohoo <yao460733480@outlook.com>
2019-03-23 18:12:08 +0800

    fix bug

--- src/datum/datum.service.ts
+++ src/datum/datum.service.ts
@@ -191,7 +191,7 @@ export class DatumService {
     if (!user || datum.userId !== user.id) {
       throw new BadRequestException('你在做什么呢');
     }
-    datum.stay += stayTime;
+    datum.stay += stayTime * 1;
     return await this.simpleDataRepository.save(datum);
   }
 }
