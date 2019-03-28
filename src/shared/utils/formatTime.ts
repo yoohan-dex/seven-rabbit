a654a755721394462f1b1602514cce3017054319
Yoohoo <yao460733480@outlook.com>
2019-03-28 13:11:08 +0800

    reupload

--- src/shared/utils/formatTime.ts
+++
@@ -1,8 +0,0 @@
-const format = (t: string) => {
-  return new Date(parseInt(t, 10))
-    .toJSON()
-    .replace('T', ' ')
-    .replace('Z', '');
-};
-
-export { format };
--- src/shared/utils/ramdom.ts
+++
@@ -1,5 +0,0 @@
-export const random = (l: number) => {
-  const r = parseInt((Math.random() * 1000000).toFixed(), 10);
-  const n = r % l;
-  return n;
-};
