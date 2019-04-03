cc3883998d72c73036230ce7acbab962ada7a448
Yoohoo <yao460733480@outlook.com>
2019-04-03 15:52:06 +0800

    test

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -63,7 +63,7 @@ export class ProductService {
         : sortObj.productIds;
     console.time('get hot list');
     const hotList = await this.productRepository.find({
-      select: ['cover', 'id', 'cover'],
+      select: ['cover', 'id'],
       where: {
         hot: true,
         id: In(sortIds),
