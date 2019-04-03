673d5262291725829bd5531b4155ee65cbf44310
Yoohoo <yao460733480@outlook.com>
2019-04-03 14:34:49 +0800

    emm

--- src/topic/topic.module.ts
+++ src/topic/topic.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TopicController } from './topic.controller';
 import { TopicService } from './topic.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Image } from 'common/common.entity';
+import { Image } from '../common/common.entity';
 import { Topic } from './topic.entity';
 
 @Module({
