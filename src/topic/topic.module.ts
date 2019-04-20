8d9983e15e9df614865f6cf930c59b7b12f92e19
Yoohoo <yao460733480@outlook.com>
2019-04-20 21:58:45 +0800

    emm

--- src/topic/topic.module.ts
+++ src/topic/topic.module.ts
@@ -5,9 +5,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Image } from '../common/common.entity';
 import { Topic, TopicSort } from './topic.entity';
 import { DatumService } from '../datum/datum.service';
+import { SimpleData, TopicData } from '../datum/datum.entity';
+import { WxUser } from '../auth/auth.entity';
+import { Product } from '../product/product.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Image, Topic, TopicSort])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Image,
+      Topic,
+      TopicSort,
+      SimpleData,
+      WxUser,
+      Product,
+      TopicData,
+    ]),
+  ],
   controllers: [TopicController],
   providers: [TopicService, DatumService],
 })
