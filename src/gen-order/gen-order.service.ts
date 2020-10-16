609c5b264555f52adbd8b31795e4b91bdf60a091
Yoohoo <yao460733480@outlook.com>
2020-10-16 16:56:35 +0800

    emm

--- src/gen-order/gen-order.service.ts
+++ src/gen-order/gen-order.service.ts
@@ -313,9 +313,6 @@ export class GenOrderService {
               );
               colorAndCount[rule.size] = c;
               // tslint:disable-next-line:no-construct
-            } else if (rr.size !== rule.size) {
-              const c = this.parseCount2XML('', `+${rr.count}`);
-              colorAndCount[rule.size] = c;
             } else if (new String(colorAndCount[rule.size]).length < 10) {
               colorAndCount[rule.size] = this.parseCount2XML(
                 `${rule.count === 0 ? '' : rule.count}`,
