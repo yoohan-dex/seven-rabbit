ba195c15e473a8e2f31b8a8cf287d6e7d70d8c40
NUC <nuc@192.168.0.101>
2021-09-08 20:08:27 +0800

    ss

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,13 +3,13 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  // @Get('back-admin')
-  // @Render('bundle')
-  // root() {}
+  @Get('back-admin')
+  @Render('back-admin/index')
+  root() { }
 
   @Get('super-word-generator')
   @Render('bundle')
-  wordGenerator() {}
+  wordGenerator() { }
 }
